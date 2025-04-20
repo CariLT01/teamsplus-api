a0c1fb0bf8b735f48740d8cadfd974b33c177a84
Cari Peng <caripeng30@gmail.com>
2025-04-19 21:57:26 -0400

    use webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        'static/login/script': './static/login/script.ts',
+        'static/register/script': './static/register/script.ts',
+        'static/dashboard/script': './static/dashboard/script.ts',
+        'static/home/script': './static/home/script.ts',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname),
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    cache: {
+        type: 'filesystem',  // Enable filesystem caching
+        buildDependencies: {
+          config: [__filename],  // Cache invalidates if the config file changes
+        },
+      },
+    mode: 'production', // Set the mode to production for optimizations
+};
